6efd27c90d78c3c3941dacdcca5607736cf05802
龚虹宇 <gonghongyu@le.com>
2017-08-20 21:18:13 +0800

    merge

---
+++ page/share/chrome/test/a.js
@@ -0,0 +1,12 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+//var t1=+new Date();
+// while(+new Date()-t1<10000){
+//     
+// }
+
+console.log("mmmmmm")
